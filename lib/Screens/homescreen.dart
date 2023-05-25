3e25205fabbdf799a8fa90f8fe0fc23e7a1df0e5
MohmedAshraf <92545354+MohamedAshraf701@users.noreply.github.com>
2023-05-25 20:59:58 +0530

    getx

---
+++ lib/Screens/homescreen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class MyScreen extends StatefulWidget {
+  @override
+  _MyScreenState createState() => _MyScreenState();
+}
+
+class _MyScreenState extends State<MyScreen> {
+  String selectedType = '';
+  bool isChecked = false;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+
+  List<String> types = [
+    'Type A',
+    'Type B',
+    'Type C',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Screen'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Price',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            TextField(
+              controller: priceController,
+              decoration: InputDecoration(
+                hintText: 'Enter price',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Types',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            DropdownButton<String>(
+              value: selectedType,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedType = newValue!;
+                });
+              },
+              items: types.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 16.0),
+            Row(
+              children: [
+                Checkbox(
+                  value: isChecked,
+                  onChanged: (bool? newValue) {
+                    setState(() {
+                      isChecked = newValue!;
+                    });
+                  },
+                ),
+                Text('Check me'),
+              ],
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Name',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                hintText: 'Enter name',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
